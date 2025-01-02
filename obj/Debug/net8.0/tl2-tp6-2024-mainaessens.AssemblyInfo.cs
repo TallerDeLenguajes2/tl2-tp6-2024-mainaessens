e0b1250db2c864b16e43f0175278d88b065064e6
Maia Naessens <naessensmai@gmail.com>
2025-01-02 17:09:43 -0300

    vistas de presupuesto

--- obj/Debug/net8.0/tl2-tp6-2024-mainaessens.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp6-2024-mainaessens.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp6-2024-mainaessens")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26199e85e468b5ce145fc8839ee2c839c0fea551")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77a0bf681b5928e31c07f8c1176f32c4f4afeb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp6-2024-mainaessens")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp6-2024-mainaessens")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
